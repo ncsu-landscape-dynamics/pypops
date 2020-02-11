aa49cf59b9a3687e26d213ae9c2514f63640c5a0
Vaclav Petras <wenzeslaus@gmail.com>
2020-02-11 16:24:41 -0500

    Slightly improve test naming and structure

--- tests/test_simulation_function_principles.py
+++ tests/test_simulation_function_principles.py
@@ -4,17 +4,17 @@ import numpy as np
 import _pypops as pypops
 
 
+def simulation2_wrapper(infected):
+    b = pypops.test_simulation2(infected)
+    result = np.array(b, copy=False)
+    return result
+
+
 def test_simplified_simulation_function():
     int_type = pypops.get_integer_raster_scalar_type()
-    float_type = pypops.get_float_raster_scalar_type()
 
     infected = np.array([[5, 0, 0],
                           [0, 0, 0]], dtype=int_type)
-
-    def simulation2_wrapper(infected):
-        b = pypops.test_simulation2(infected)
-        result = np.array(b, copy=False)
-        return result
 
     result = simulation2_wrapper(infected)
 
