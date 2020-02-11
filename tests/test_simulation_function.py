2cb2ecae685684a10c7d4b8254308b69e1e5d6bb
Vaclav Petras <wenzeslaus@gmail.com>
2020-02-11 13:25:40 -0500

    Use numpy.array not matrix

--- tests/test_simulation_function.py
+++ tests/test_simulation_function.py
@@ -6,13 +6,19 @@ def test_simulation_function():
     int_type = pypops.get_integer_raster_scalar_type()
     float_type = pypops.get_float_raster_scalar_type()
 
-    infected = np.matrix("5 0 0; 0 0 0", dtype=np.int32)
-    mortality_tracker = np.matrix("0 0 0; 0 0 0", dtype=int_type)
-    dispersers = np.matrix("0 0 0; 0 0 0", dtype=int_type)
-    susceptible = np.matrix("10 6 20; 14 15 20", dtype=int_type)
-    total_plants = np.matrix("15 6 20; 14 15 25", dtype=int_type)
-    temperature = np.matrix("5 0 5; 0 0 5", dtype=float_type)
-    weather_coefficient = np.matrix("0.6 0.8 0.7; 0.2 0.8 0.5", dtype=float_type)
+    infected = np.array([[5, 0, 0],
+                         [0, 0, 0]], dtype=np.int32)
+    mortality_tracker = np.array([[0, 0, 0],
+                                  [0, 0, 0]], dtype=int_type)
+    # dispersers = np.matrix("0 0 0; 0 0 0", dtype=int_type)
+    susceptible = np.array([[10, 6, 20],
+                            [14, 15, 20]], dtype=int_type)
+    total_plants = np.array([[15, 6, 20],
+                            [14, 15, 25]], dtype=int_type)
+    temperature = np.array([[5, 0, 5],
+                            [0, 0, 5]], dtype=float_type)
+    weather_coefficient = np.array([[0.6, 0.8, 0.7],
+                                    [0.2, 0.8, 0.5]], dtype=float_type)
 
     a = pypops.test_simulation(
         random_seed=42,
@@ -23,11 +29,11 @@ def test_simulation_function():
         susceptible=susceptible,
         total_plants=total_plants,
         mortality_tracker=mortality_tracker,
-        #dispersers=dispersers,
+        # dispersers=dispersers,
         weather=True,
         temperature=[temperature, temperature],
         weather_coefficient=[weather_coefficient, weather_coefficient],
-        #weather_coefficient=weather_coefficient,
+        # weather_coefficient=weather_coefficient,
         ew_res=100.,
         ns_res=100.,
         reproductive_rate=400.4,
@@ -44,7 +50,7 @@ def test_simulation_function():
         )
 
     print(a)
-    #print(a.infected)
+    # print(a.infected)
     print(len(a.outside_dispersers))
     print(mortality_tracker)
 
