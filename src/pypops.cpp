e25190a67870c89919249c4b623a7c8a53b26694
Vaclav Petras <wenzeslaus@gmail.com>
2020-09-16 11:39:57 -0400

    Update C++ formatting to PoPS Core standard and check in CI

--- src/pypops.cpp
+++ src/pypops.cpp
@@ -40,43 +40,41 @@ FloatRaster return_new_raster(double start_number)
 
 struct Result
 {
-    //IntegerRaster infected;
-    //IntegerRaster susceptible;
+    // IntegerRaster infected;
+    // IntegerRaster susceptible;
     std::vector<std::tuple<int, int>> outside_dispersers;
 };
-
 
 // TODO: an output type seems to be reasonable
 // or a higher level object for the model/simulation
 // or the customizable output functions will take care of it by a custom
 // context which can be these vectors or write functions
 Result test_simulation(
-        int random_seed,
-        bool use_lethal_temperature,
-        double lethal_temperature,
-        IntegerRaster infected,
-        IntegerRaster susceptible,
-        std::vector<IntegerRaster> exposed,
-        IntegerRaster total_plants,
-        std::vector<IntegerRaster> mortality_tracker,
-        IntegerRaster died,
-        bool weather,
-        std::vector<FloatRaster> temperatures,
-        std::vector<FloatRaster> weather_coefficient,
-        double ew_res,
-        double ns_res,
-        double reproductive_rate,
-        std::string natural_kernel_type,
-        double natural_scale,
-        std::string natural_direction,
-        double natural_kappa,
-        bool use_anthropogenic_kernel,
-        double percent_natural_dispersal,
-        std::string anthro_kernel_type,
-        double anthro_scale,
-        std::string anthro_direction,
-        double anthro_kappa
-        )
+    int random_seed,
+    bool use_lethal_temperature,
+    double lethal_temperature,
+    IntegerRaster infected,
+    IntegerRaster susceptible,
+    std::vector<IntegerRaster> exposed,
+    IntegerRaster total_plants,
+    std::vector<IntegerRaster> mortality_tracker,
+    IntegerRaster died,
+    bool weather,
+    std::vector<FloatRaster> temperatures,
+    std::vector<FloatRaster> weather_coefficient,
+    double ew_res,
+    double ns_res,
+    double reproductive_rate,
+    std::string natural_kernel_type,
+    double natural_scale,
+    std::string natural_direction,
+    double natural_kappa,
+    bool use_anthropogenic_kernel,
+    double percent_natural_dispersal,
+    std::string anthro_kernel_type,
+    double anthro_scale,
+    std::string anthro_direction,
+    double anthro_kappa)
 {
     IntegerRaster dispersers(infected.rows(), infected.cols(), 0);
     IntegerRaster zeros(infected.rows(), infected.cols(), 0);
@@ -118,23 +116,23 @@ Result test_simulation(
     Model<IntegerRaster, FloatRaster, FloatRaster::IndexType> model(config);
     for (unsigned step = 0; step < config.scheduler().get_num_steps(); ++step) {
         model.run_step(
-                    step,
-                    infected,
-                    susceptible,
-                    total_plants,
-                    dispersers,
-                    exposed,
-                    mortality_tracker,
-                    died,
-                    temperatures,
-                    weather_coefficient[step],
-                    treatments,
-                    resistant,
-                    outside_dispersers,
-                    spread_rate,
-                    quarantine,
-                    quarantine_areas,
-                    movements);
+            step,
+            infected,
+            susceptible,
+            total_plants,
+            dispersers,
+            exposed,
+            mortality_tracker,
+            died,
+            temperatures,
+            weather_coefficient[step],
+            treatments,
+            resistant,
+            outside_dispersers,
+            spread_rate,
+            quarantine,
+            quarantine_areas,
+            movements);
     }
     return {outside_dispersers};
 }
@@ -144,92 +142,90 @@ Result test_simulation(
 // or the customizable output functions will take care of it by a custom
 // context which can be these vectors or write functions
 Result test_simulation_wrapper(
-        int random_seed,
-        bool use_lethal_temperature,
-        double lethal_temperature,
-        py::buffer infected,
-        py::buffer susceptible,
-        std::vector<py::buffer> exposed,
-        py::buffer total_plants,
-        std::vector<py::buffer> mortality_tracker,
-        py::buffer died,
-        bool weather,
-        std::vector<py::buffer> temperature,
-        std::vector<py::buffer> weather_coefficient,
-        double ew_res,
-        double ns_res,
-        double reproductive_rate,
-        std::string natural_kernel_type,
-        double natural_scale,
-        std::string natural_direction,
-        double natural_kappa,
-        bool use_anthropogenic_kernel,
-        double percent_natural_dispersal,
-        std::string anthro_kernel_type,
-        double anthro_scale,
-        std::string anthro_direction,
-        double anthro_kappa
-        )
+    int random_seed,
+    bool use_lethal_temperature,
+    double lethal_temperature,
+    py::buffer infected,
+    py::buffer susceptible,
+    std::vector<py::buffer> exposed,
+    py::buffer total_plants,
+    std::vector<py::buffer> mortality_tracker,
+    py::buffer died,
+    bool weather,
+    std::vector<py::buffer> temperature,
+    std::vector<py::buffer> weather_coefficient,
+    double ew_res,
+    double ns_res,
+    double reproductive_rate,
+    std::string natural_kernel_type,
+    double natural_scale,
+    std::string natural_direction,
+    double natural_kappa,
+    bool use_anthropogenic_kernel,
+    double percent_natural_dispersal,
+    std::string anthro_kernel_type,
+    double anthro_scale,
+    std::string anthro_direction,
+    double anthro_kappa)
 {
     return test_simulation(
-                random_seed,
-                use_lethal_temperature,
-                lethal_temperature,
-                py_buffer_info_to_raster<IntegerRaster>(infected),
-                py_buffer_info_to_raster<IntegerRaster>(susceptible),
-                py_buffers_info_to_rasters<IntegerRaster>(exposed),
-                py_buffer_info_to_raster<IntegerRaster>(total_plants),
-                py_buffers_info_to_rasters<IntegerRaster>(mortality_tracker),
-                py_buffer_info_to_raster<IntegerRaster>(died),
-                weather,
-                py_buffers_info_to_rasters<FloatRaster>(temperature),
-                py_buffers_info_to_rasters<FloatRaster>(weather_coefficient),
-                ew_res,
-                ns_res,
-                reproductive_rate,
-                natural_kernel_type,
-                natural_scale,
-                natural_direction,
-                natural_kappa,
-                use_anthropogenic_kernel,
-                percent_natural_dispersal,
-                anthro_kernel_type,
-                anthro_scale,
-                anthro_direction,
-                anthro_kappa
-            );
+        random_seed,
+        use_lethal_temperature,
+        lethal_temperature,
+        py_buffer_info_to_raster<IntegerRaster>(infected),
+        py_buffer_info_to_raster<IntegerRaster>(susceptible),
+        py_buffers_info_to_rasters<IntegerRaster>(exposed),
+        py_buffer_info_to_raster<IntegerRaster>(total_plants),
+        py_buffers_info_to_rasters<IntegerRaster>(mortality_tracker),
+        py_buffer_info_to_raster<IntegerRaster>(died),
+        weather,
+        py_buffers_info_to_rasters<FloatRaster>(temperature),
+        py_buffers_info_to_rasters<FloatRaster>(weather_coefficient),
+        ew_res,
+        ns_res,
+        reproductive_rate,
+        natural_kernel_type,
+        natural_scale,
+        natural_direction,
+        natural_kappa,
+        use_anthropogenic_kernel,
+        percent_natural_dispersal,
+        anthro_kernel_type,
+        anthro_scale,
+        anthro_direction,
+        anthro_kappa);
 }
 
 // TODO: an output type seems to be reasonable
 // or a higher level object for the model/simulation
 // or the customizable output functions will take care of it by a custom
 // context which can be these vectors or write functions
-IntegerRaster test_simulation2(
-    IntegerRaster infected
-)
+IntegerRaster test_simulation2(IntegerRaster infected)
 {
     infected(1, 1) = 200;
     auto result = 5 * infected;
     return result;
 }
 
-IntegerRaster test_simulation2_buffer_wrapper(py::buffer b) {
+IntegerRaster test_simulation2_buffer_wrapper(py::buffer b)
+{
     py::buffer_info info = b.request();
-    if (info.format == py::format_descriptor<Integer>::format() && info.itemsize == sizeof(Integer)) {
+    if (info.format == py::format_descriptor<Integer>::format()
+        && info.itemsize == sizeof(Integer)) {
         raster_compatible_or_throw<Integer>(info);
-        return test_simulation2(IntegerRaster(static_cast<Integer*>(info.ptr), info.shape[0], info.shape[1]));
+        return test_simulation2(IntegerRaster(
+            static_cast<Integer*>(info.ptr), info.shape[0], info.shape[1]));
     }
     raster_compatible_or_throw<Integer>(info);
     throw std::logic_error("Raster-array scalar type incompatibility not identified");
 }
 
-PYBIND11_MODULE(_pypops, m) {
+PYBIND11_MODULE(_pypops, m)
+{
     m.doc() = "C++ PoPS wrapper for PyPoPS";
 
-    m.def("get_float_raster_scalar_type",
-          &get_float_raster_scalar_type);
-    m.def("get_integer_raster_scalar_type",
-          &get_integer_raster_scalar_type);
+    m.def("get_float_raster_scalar_type", &get_float_raster_scalar_type);
+    m.def("get_integer_raster_scalar_type", &get_integer_raster_scalar_type);
 
     raster_class<FloatRaster>(m, "FloatRaster");
     raster_class<IntegerRaster>(m, "IntegerRaster");
@@ -250,68 +246,73 @@ PYBIND11_MODULE(_pypops, m) {
     // pypops.test_compatibility_long(np.array([[2,1,0], [4,6,7]], dtype=np.long))
     // expected q, got l (both have 8 bytes, using C++ type with id: l)
 
-    m.def("return_new_raster",
-          &return_new_raster,
-          "start_number"_a);
+    m.def("return_new_raster", &return_new_raster, "start_number"_a);
 
-    m.def("modify_existing_raster",
-          [](py::buffer b, Float x) {
+    m.def("modify_existing_raster", [](py::buffer b, Float x) {
         // we convert Float scalar to Integer when needed
         py::buffer_info info = b.request();
-        if (info.format == py::format_descriptor<Float>::format() && info.itemsize == sizeof(Float)) {
+        if (info.format == py::format_descriptor<Float>::format()
+            && info.itemsize == sizeof(Float)) {
             raster_compatible_or_throw<Float>(info);
-            modify_existing_raster(FloatRaster(static_cast<Float*>(info.ptr), info.shape[0], info.shape[1]), x);
-        } else if (info.format == py::format_descriptor<Integer>::format() && info.itemsize == sizeof(Integer)) {
+            modify_existing_raster(
+                FloatRaster(
+                    static_cast<Float*>(info.ptr), info.shape[0], info.shape[1]),
+                x);
+        }
+        else if (
+            info.format == py::format_descriptor<Integer>::format()
+            && info.itemsize == sizeof(Integer)) {
             raster_compatible_or_throw<Integer>(info);
-            modify_existing_raster(IntegerRaster(static_cast<Integer*>(info.ptr), info.shape[0], info.shape[1]), Integer(x));
-        } else {
+            modify_existing_raster(
+                IntegerRaster(
+                    static_cast<Integer*>(info.ptr), info.shape[0], info.shape[1]),
+                Integer(x));
+        }
+        else {
             // throw
             raster_compatible_or_throw<Float>(info);
         }
     });
 
-//    pybind11::class_<Config>(m, "Config");
-//    pybind11::class_<Model>(m, "Model").
-//            .def_readwrite("random_seed", &Config::random_seed);
+    //    pybind11::class_<Config>(m, "Config");
+    //    pybind11::class_<Model>(m, "Model").
+    //            .def_readwrite("random_seed", &Config::random_seed);
 
     py::class_<Result>(m, "Result")
-             //.def_readonly("infected", &Result::infected)
-             //.def_readonly("susceptible", &Result::susceptible)
-             .def_readonly("outside_dispersers", &Result::outside_dispersers)
-            ;
-
+        //.def_readonly("infected", &Result::infected)
+        //.def_readonly("susceptible", &Result::susceptible)
+        .def_readonly("outside_dispersers", &Result::outside_dispersers);
 
     // Tip: if you get "number of annotations does not match"
     // but parameters seems to be right, check the commas.
-    m.def("test_simulation",
-          &test_simulation_wrapper,
-          "random_seed"_a,
-          "use_lethal_temperature"_a,
-          "lethal_temperature"_a,
-          "infected"_a,
-          "susceptible"_a,
-          "exposed"_a,
-          "total_plants"_a,
-          "mortality_tracker"_a,
-          "died"_a,
-          "weather"_a,
-          "temperature"_a,
-          "weather_coefficient"_a,
-          "ew_res"_a,
-          "ns_res"_a,
-          "reproductive_rate"_a,
-          "natural_kernel_type"_a,
-          "natural_scale"_a,
-          "natural_direction"_a,
-          "natural_kappa"_a,
-          "use_anthropogenic_kernel"_a,
-          "percent_natural_dispersal"_a,
-          "anthro_kernel_type"_a,
-          "anthro_scale"_a,
-          "anthro_direction"_a,
-          "anthro_kappa"_a
-          );
+    m.def(
+        "test_simulation",
+        &test_simulation_wrapper,
+        "random_seed"_a,
+        "use_lethal_temperature"_a,
+        "lethal_temperature"_a,
+        "infected"_a,
+        "susceptible"_a,
+        "exposed"_a,
+        "total_plants"_a,
+        "mortality_tracker"_a,
+        "died"_a,
+        "weather"_a,
+        "temperature"_a,
+        "weather_coefficient"_a,
+        "ew_res"_a,
+        "ns_res"_a,
+        "reproductive_rate"_a,
+        "natural_kernel_type"_a,
+        "natural_scale"_a,
+        "natural_direction"_a,
+        "natural_kappa"_a,
+        "use_anthropogenic_kernel"_a,
+        "percent_natural_dispersal"_a,
+        "anthro_kernel_type"_a,
+        "anthro_scale"_a,
+        "anthro_direction"_a,
+        "anthro_kappa"_a);
 
-     m.def("test_simulation2",
-           &test_simulation2_buffer_wrapper);
+    m.def("test_simulation2", &test_simulation2_buffer_wrapper);
 }
